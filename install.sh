ef310df0cd8966f0b6c47edae4500b5a86adc7cd
Mehdi Cherti <mehdicherti@gmail.com>
2019-01-13 11:58:57 +0100

    update

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mv ~/.vim ~/.vim.bak
-mv ~/.vimrc ~/.vimrc.bak
-mv ~/.tmux.conf ~/.tmux.conf.bak
-mv ~/.tmux ~/.tmux.bak
-mv ~/.profile ~/.profile.bak
-ln -sf .vim .vimrc .tmux.conf .tmux .profile  ~
